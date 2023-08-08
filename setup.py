ed2d418da1fe9cf96774ef5d7381c022c570fb81
Chris Markiewicz <effigies@gmail.com>
2023-08-08 16:33:26 -0400

    ENH: Configure isolated builds

--- setup.py
+++ setup.py
@@ -1,53 +1,21 @@
-from numpy.distutils.core import setup, Extension
-from numpy.distutils.misc_util import get_numpy_include_dirs 
-import os, sys
+"""Setup gradunwarp
 
-mods = ['gradunwarp.core.coeffs', 'gradunwarp.core.globals', 
-        'gradunwarp.core.__init__', 'gradunwarp.__init__',
-        'gradunwarp.core.utils',
-        'gradunwarp.core.unwarp_resample',
-        'gradunwarp.core.gradient_unwarp',
-        'gradunwarp.core.tests.test_utils',
-        'gradunwarp.core.tests.test_gradient_unwarp',
-       ]
+Most configuration is now in pyproject.toml. This file configures
+extensions and a legacy script.
+"""
+from setuptools import setup, Extension
+from numpy import get_include
 
-dats = [('gradunwarp/core/', ['gradunwarp/core/interp3_ext.c']),
-        ('gradunwarp/core/', ['gradunwarp/core/legendre_ext.c']),
-        ('gradunwarp/core/', ['gradunwarp/core/transform_coordinates_ext.c']),
-       ]
 
-# to build the C extension interp3_ext.c
-ext1 = Extension('gradunwarp.core.interp3_ext',
-                 include_dirs = get_numpy_include_dirs(),
-                 sources = ['gradunwarp/core/interp3_ext.c'],
-                 extra_compile_args=['-O3'])
-# to build the C extension legendre_ext.c
-ext2 = Extension('gradunwarp.core.legendre_ext',
-                 include_dirs = get_numpy_include_dirs(),
-                 sources = ['gradunwarp/core/legendre_ext.c'],
-                 extra_compile_args=['-O3'])
-# to build the C extension transform_coordinates_ext.c
-ext3 = Extension('gradunwarp.core.transform_coordinates_ext',
-                 include_dirs = get_numpy_include_dirs(),
-                 sources = ['gradunwarp/core/transform_coordinates_ext.c'],
-                 extra_compile_args=['-O3'])
-
-scripts_cmd = ['gradunwarp/core/gradient_unwarp.py',]
-        
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('',parent_package,top_path)
-    config.add_data_files ( *dats )
-    return config
-
-setup(name='gradunwarp',
-      version = '1.2.1+HCP',
-      description = 'HCP version of Gradient Unwarping Package for Python/Numpy',
-      author = 'Human Connectome Project',
-      py_modules  = mods,
-      ext_modules = [ext1, ext2, ext3],
-      scripts = scripts_cmd,
-      configuration=configuration,
-     )
-
+setup(
+    ext_modules=[
+        Extension(
+            f'gradunwarp.core.{mod}_ext',
+            include_dirs=[get_include()],
+            sources=[f'gradunwarp/core/{mod}_ext.c'],
+            extra_compile_args=['-O3'],
+        )
+        for mod in ('interp3', 'legendre', 'transform_coordinates')
+    ],
+    scripts=['gradunwarp/core/gradient_unwarp.py'],
+)
