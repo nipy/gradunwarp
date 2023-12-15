6c9df6fcbe733812dcad1834971cd9e7e1f4f64c
Tim Coalson <coalsont@wustl.edu>
2023-12-15 15:42:19 -0600

    bump version 1.2.2.dev -> 1.2.2

--- gradunwarp/core/globals.py
+++ gradunwarp/core/globals.py
@@ -6,7 +6,7 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 import logging
 
-VERSION = '1.2.2.dev'
+VERSION = '1.2.2'
 
 usage = '''
 gradient_unwarp infile outfile manufacturer -g <coefficient file> [optional arguments]
